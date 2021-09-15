ba1cb26b000d19c1e3e57ca6b37f5977fbfd2f23
n-ottavi-n <nottavi14@gmail.com>
2021-09-15 20:31:45 +0200

    Update Atelier4_ex1.py

--- Atelier4_ex1.py
+++ Atelier4_ex1.py
@@ -20,6 +20,25 @@ def full_name(str_arg:str)->str:
         nom correctement formaté
 
     """
+    lettre=str_arg[0]
+    res=""#nouvelle string
+    i=0
+    while lettre!=' ':#boucle jusq'a atteindre l'espace
+        res+=lettre.upper()#ajout des majuscules une par une
+        i+=1
+        lettre=str_arg[i]
+    #i=indice de l'espace
+    res+=str_arg[i]#ajout de l'espace
+    lettre=str_arg[i+1]# recupere la premiere lettre du prenom
+    res+=lettre.upper()#premiere lettre du prenom pass en majuscule
+    res+=str_arg[i+2:]#ajout du reste d ela string d'entree
+    return res
+
+nom="ottavi nicolas"
+print(full_name(nom))
+
+
+    
     
     
     
